d0fc0d725fb08db19e5dae8feb18432c8c8dc5c4
FedeV1601 <68011677+FedeV1601@users.noreply.github.com>
2021-04-28 11:01:42 -0300

    Falta Index y Creditos
    Funciona juego.html
    Falta vincular pagina de bienvenida y creditos

--- scripts.js
+++ scripts.js
@@ -1,16 +1,18 @@
 String.prototype.replaceAt=function(index, character) 
 { return this.substr(0, index) + character + this.substr(index+character.length); }
 
-
-const palabras=["murcielago", "casa","perro", "gato", "correspondencia", "otorrinolaringologo", "vehiculo", "silencio", "puerta", "ascensor" ];
+const palabras=["murcielago", "casa","perro", "gato", "telefono", "felicitas", "Benjamin", "francisco", "pimpinela", "vehiculo", "silencio", "puerta", "ascensor" ];
 const palabraSecreta= palabras[Math.floor(Math.random()*palabras.length)];
 let palabraConGuiones =palabraSecreta.replace(/./g, "- ");
 let contadorFallos = 0;
 
 document.querySelector("#output").innerHTML = palabraConGuiones;
 document.querySelector("#cantLetras").innerHTML = "La Palabra tiene " + palabraSecreta.length + " letras";
-//document.querySelector("#errada").innerHTML = erradas.value;
-document.querySelector("#enviar").addEventListener("click",()=> {
+let erradas = [];
+
+
+
+document.querySelector("#jugar").addEventListener("click",()=> {
     
     const letra = document.querySelector("#letra").value;
     
@@ -24,30 +26,66 @@ document.querySelector("#enviar").addEventListener("click",()=> {
         }
         
     }
-
+    
     if(haFallado)
     {
         contadorFallos++;
-    /*     let erradas =[letra];
-        document.querySelector("#errada").innerHTML = erradas.value; */
+    
+        let letraMay = letra.toUpperCase();
+                erradas.push(" " + letraMay);
+       
+        document.querySelector("#errada").innerHTML = "Fallaste " + contadorFallos + " letras " + erradas;
+
+
         document.querySelector("#ahorcado").style.backgroundPosition = -(150*contadorFallos) + 'px 0';
-        if(contadorFallos>=6)
+        if(contadorFallos==6)
         {
-            alert("Perdiste el Juego")
+            var resultado = window.confirm('Perdiste!!!! Queres jugar de nuevo?');
+            if (resultado === true) {
+                setTimeout(reset, 1000);
+                function reset(){document.location.reload();}
+            } 
+            else{
+
+                window.alert("Será entonces hasta la próxima!!");
+            }
         }
     }else{
 
         if(palabraConGuiones.indexOf("-")<0)
         {
-            alert("GANASTE!!")
+            var resultado = window.confirm('Ganaste !!!! Queres jugar de nuevo?');
+                if (resultado === true) {
+                    setTimeout(reset, 1000);
+                    function reset(){document.location.reload();}
+                }   else{
+
+                    window.alert("Será entonces hasta la próxima!!");
+                } 
+
+               
+            
         }
 
     }
-  
+ 
     document.querySelector("#cantLetras").innerHTML = "La palabra tiene " + palabraSecreta.length + " letras";
     document.querySelector("#output").innerHTML = palabraConGuiones;
     document.querySelector("#letra").value="";
     document.querySelector("#letra").focus();
 
 });
+
+/* function setGameOver() {
+   
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Iniciar nuevo juego';
+    document.body.append(resetButton);
+    resetButton.addEventListener('click', resetGame);
+  }
+
+  function resetGame() {
+    document.location.reload();
+ 
+  } */
 
