20843b38a8c4ced40373dbddf488956da1a8a3f5
FedeV1601 <68011677+FedeV1601@users.noreply.github.com>
2021-05-05 13:57:10 -0300

    Codigo comentado + Readme

---
+++ README.txt
@@ -0,0 +1,4 @@
+El juego se trata de que el jugador adivine la palabra secreta eligiendo de a una las letras que la componen.
+Cuenta con la posibilidad de fallar 5 veces, a la 6ta vez, pierde el juego.
+El universo de palabras posibles está guardado en un array, y de forma aleatoria la PC 
+elige una de ellas para cada vez que se inicia el juego.
--- scripts.js
+++ scripts.js
@@ -1,17 +1,19 @@
 String.prototype.replaceAt=function(index, character) 
-{ return this.substr(0, index) + character + this.substr(index+character.length); }
+{ return this.substr(0, index) + character + this.substr(index+character.length); } //Debo declarar esta funcion para que funcione el llamado al metodo replaceAt() tal como la necesitamos
 
 const words=["murcielago", "casa","perro", "gato", "telefono", "felicitas", "Benjamin", "francisco", "pimpinela", "vehiculo", "silencio", "puerta", "ascensor" ];
-const secretWord= words[Math.floor(Math.random()*words.length)];
-let hyphenatedWord =secretWord.replace(/./g, "- ");
-let faultsCounter = 0;
+const secretWord= words[Math.floor(Math.random()*words.length)];//Elijo de manera aleatoria del array "words" una de las palabras que será la palabra secreta 
+let hyphenatedWord =secretWord.replace(/./g, "- "); // para poder mostrar la palabra con guiones utilizo el metodo replace() que reemplazará cada una de las letras de la palabra secreta por un guion y un espacio
+let faultsCounter = 0; // contador de palabras erradas
 
-document.querySelector("#output").innerHTML = hyphenatedWord;
-document.querySelector("#amountOfLetters").innerHTML = "La Palabra tiene " + secretWord.length + " letras";
-let faults = [];
+document.querySelector("#output").innerHTML = hyphenatedWord; // En el elemento con id "output" se mostrará la palabra con guiones
+document.querySelector("#amountOfLetters").innerHTML = "La Palabra tiene " + secretWord.length + " letras"; // En el elemento con id "amountOfLetters" se mostrará la cantidad de letras de la palabra secreta
+let faults = []; // declaro un array en el que luego guardaré las letras seleccionadas por el usuario que no eran parte de la palabra secreta
+
+// FUNCIONES DE LA BARRA DE NAVEGACION
 
 function letsPlay(){
-    window.location.href="game.html";
+    window.location.href="game.html"; 
 }
 function index() {
     window.location.href="index.html";
@@ -20,61 +22,59 @@ function credits() {
     window.location.href="credits.html";
 }
 
-
+// FUNCION PRINCIPAL DEL JUEGO
 document.querySelector("#letsPlay").addEventListener("click",()=> {
     
     const letter = document.querySelector("#letter").value;
     
     let failed =true;
-    for(const i in secretWord) //recorre el string como si fuera un array?
+    for(const i in secretWord)
     {
         if(letter == secretWord[i])
         {
-            hyphenatedWord = hyphenatedWord.replaceAt(i*2, letter);
+            hyphenatedWord = hyphenatedWord.replaceAt(i*2, letter); // reemplazo las letras adivinadas por el usuario en la palabra secreta. El i*2 es porquie hay un espacio entre letra y letra
             failed= false;
         }
-        
     }
     
     if(failed)
     {
         faultsCounter++;
     
-        let letterMay = letter.toUpperCase();
-                faults.push(" " + letterMay);
+        let capsLetter = letter.toUpperCase();
+                faults.push(" " + capsLetter); // Guardo en el arary las letras erradas en letra mayúscula
        
-        document.querySelector("#fault").innerHTML = "Fallaste " + faultsCounter + " letras " + faults;
+        document.querySelector("#fault").innerHTML = "Fallaste " + faultsCounter + " letras: " + faults;
 
 
-        document.querySelector("#hangMan").style.backgroundPosition = -(150*faultsCounter) + 'px 0';
+        document.querySelector("#hangMan").style.backgroundPosition = -(150*faultsCounter) + 'px 0'; // Con esta sentencia voy moviendo el backgroun para mmostrar la imagen de acuerdo a la cantidad de errores del jugador
         if(faultsCounter==6)
         {
           
             var result = window.confirm('Perdiste!!!! Queres jugar de nuevo?');
             if (result === true) {
               setTimeout(reset, 1000);
-                function reset(){document.location.reload();}
+                function reset(){document.location.reload();} //Si el jugador quiere jugar de nuevo, recargo la página
             } 
             else{
 
-                window.alert("Será entonces hasta la próxima!!");
+                window.alert("Será entonces hasta la próxima!!"); // Si no quiere jugar de nuevo, cargo el index.html
+                index();
+                
             }
         }
     }else{
 
-        if(hyphenatedWord.indexOf("-")<0)
+        if(hyphenatedWord.indexOf("-")<0) // Si en la palabra secreta no hay mas guiones quiere decir que el jugador adivinó la palabra. El metodo indexOf devuelve -1 si no se encuentra lo que se busca, ene ste caso el "-"
         {
-            var result = window.confirm('Ganaste !!!! Queres jugar de nuevo?');
+            var result = window.confirm('Ganaste !!!! Queres jugar de nuevo?');//window confirm devuelve tru si el usuario elige "Aceptar"  o false si elige "Cancelar"
                 if (result === true) {
                     setTimeout(reset, 1000);
                     function reset(){document.location.reload();}
-                }   else{
+                }else{
 
                     window.alert("Será entonces hasta la próxima!!");
                 } 
-
-               
-            
         }
 
     }
@@ -85,17 +85,4 @@ document.querySelector("#letsPlay").addEventListener("click",()=> {
     document.querySelector("#letter").focus();
 
 });
-
-/* function setGameOver() {
-   
-    resetButton = document.createElement('button');
-    resetButton.textContent = 'Iniciar nuevo juego';
-    document.body.append(resetButton);
-    resetButton.addEventListener('click', resetGame);
-  }
-
-  function resetGame() {
-    document.location.reload();
- 
-  } */
 
